39feb9c3033e45f282bec8e80abf4486847445fd
elshaddai tambuwun <elshatambuwun17@gmail.com>
2024-11-13 18:58:27 +0700

    Update a picture in the home page in Firebase

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,19 +1,52 @@
-import React from 'react'
 import "./home.css"
 import Social from './social';
-import Intro from './Intro';
-
-
+import { database } from '../config/firebase/index';
+import { ref, onValue } from "firebase/database";
+import React, { useEffect, useState } from 'react';
 
 
 const Home = () => {
+  const [Title, setTitle] = useState("");
+  const [subtitle, setSubtitle] = useState("")
+  const [paragraph, setParagraph] = useState("");
+  const [imageBase64, setImageBase64] = useState("")
+
+  useEffect(() => {
+    const homeRef = ref(database, 'Home');
+    onValue(homeRef, (snapshot) => {
+        const data = snapshot.val();
+        console.log("Data fetched from Firebase:", data);
+        if (data) {
+            setTitle(data.Title || "");
+            setSubtitle(data.Subtitle || "");
+            setParagraph(data.Paragraph || "");
+            if (data.ElshaImg && !data.ElshaImg.startsWith('data:image')) {
+                setImageBase64(`data:image/jpeg;base64,${data.ElshaImg}`);
+            } else {
+                setImageBase64(data.ElshaImg || "");
+            }
+        }
+    });
+}, []);
   return (
     <section className="home section" id="home">
         <div className="home_container container grid">
             <div className="home_content grid">
-            <Social />
-            <Intro />
+              <div className="home_social">
+              <Social />
+              </div>
 
+              <div className="home_data">
+                <h1 className="home_title">{Title}</h1>
+                <h3 className="home_subtitle ">{subtitle}</h3>
+                <p className="home_description">{paragraph}</p>
+              </div>
+
+              <div 
+              className="home_img"
+              style={{backgroundImage: `url(${imageBase64})`,
+              backgroundColor: '#f0f0f0'
+              }}></div>
 
             </div>
         </div>
