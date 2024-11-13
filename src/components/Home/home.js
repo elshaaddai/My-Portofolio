c96a9f1923a6cb516b225957c1804f60d9895a75
elshaddai tambuwun <elshatambuwun17@gmail.com>
2024-11-13 23:39:47 +0700

    adding social-icon in firebase

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,5 +1,4 @@
 import "./home.css"
-import Social from './social';
 import { database } from '../config/firebase/index';
 import { ref, onValue } from "firebase/database";
 import React, { useEffect, useState } from 'react';
@@ -10,20 +9,34 @@ const Home = () => {
   const [subtitle, setSubtitle] = useState("")
   const [paragraph, setParagraph] = useState("");
   const [imageBase64, setImageBase64] = useState("")
+  const [igImg, setIgImg] = useState("")
+  const [linkedin, setLinkedInImg] = useState("")
+  const [github, setGithubImg] = useState("")
 
   useEffect(() => {
     const homeRef = ref(database, 'Home');
     onValue(homeRef, (snapshot) => {
         const data = snapshot.val();
         console.log("Data fetched from Firebase:", data);
+        console.log("GitHub image data:", data.githubImg);
+
+
         if (data) {
             setTitle(data.Title || "");
             setSubtitle(data.Subtitle || "");
             setParagraph(data.Paragraph || "");
-            if (data.ElshaImg && !data.ElshaImg.startsWith('data:image')) {
+
+            if (data.ElshaImg) {
                 setImageBase64(`data:image/jpeg;base64,${data.ElshaImg}`);
-            } else {
-                setImageBase64(data.ElshaImg || "");
+            } 
+            if (data.igImg) {
+              setIgImg(`data:image/png;base64,${data.igImg}`);
+            }
+            if (data.linkedinImg){
+              setLinkedInImg(`data:image/png;base64,${data.linkedinImg}`)
+            }
+            if (data.githubImg) {
+              setGithubImg(`data:image/jpeg;base64,${data.githubImg}`);
             }
         }
     });
@@ -33,7 +46,29 @@ const Home = () => {
         <div className="home_container container grid">
             <div className="home_content grid">
               <div className="home_social">
-              <Social />
+                <a 
+                href="https://www.instagram.com/elshaa.tambuwun/#" 
+                className="home-social_icon icon-instagram" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                style={{ backgroundImage: `url(${igImg})` }}>
+                </a>
+
+                <a 
+                href="https://www.linkedin.com/in/elshaddai-grace-tambuwun-355899304/" 
+                className="home-social_icon icon-linkedin" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                style={{ backgroundImage: `url(${linkedin})` }}>
+                </a>
+
+                <a 
+                href="https://github.com/elshaaddai" 
+                className="home-social_icon icon-github" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                style={{ backgroundImage: `url(${github})` }}>
+                </a>
               </div>
 
               <div className="home_data">
@@ -47,7 +82,6 @@ const Home = () => {
               style={{backgroundImage: `url(${imageBase64})`,
               backgroundColor: '#f0f0f0'
               }}></div>
-
             </div>
         </div>
     </section>
