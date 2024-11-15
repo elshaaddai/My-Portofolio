34d48ce195ba072cae057465535ef5e42fc08930
elshaddai tambuwun <elshatambuwun17@gmail.com>
2024-11-15 11:09:35 +0700

    last commit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,25 +1,105 @@
 import './contact.css'
+import { database } from '../config/firebase/index';
+import { ref, onValue } from "firebase/database";
+import React, { useEffect, useState } from 'react';
 
 function Contact() {
+  const [contactTitle, setcontactTitle] = useState("");
+  const [contactSubtitle, setcontactSubtitle] = useState("");
+  const [igImg, setIgImg] = useState("");
+  const [linkedinImg, setLinkedInImg] = useState("");
+  const [gihubImg, setGithubImg] = useState("");
+  const [emailImg, setEmailImg] = useState("");
+  const [email, setEmail] = useState("");
+  const [instagram, setInstagram] = useState("");
+  const [github, setGithub] = useState("");
+  const [linkedIn, setLinkedin] = useState("");
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const contactRef = ref(database, 'Contact');
+    onValue(contactRef, (snapshot) => {
+        const data = snapshot.val();
+        console.log("Data fetched from Firebase:", data);
+        console.log("GitHub image data:", data.gihubImg);
+
+
+        if (data) {
+            setcontactTitle(data.contactTitle || "");
+            setcontactSubtitle(data.contactSubtitle || "");
+            setEmail(data.email || "");
+            setLinkedin(data.linkedIn || "");
+            setInstagram(data.instagram || "");
+            setGithub(data.github || "");
+
+            if (data.emailImg){
+              setEmailImg(`data:image/jpeg;base64,${data.emailImg}`);
+            }
+
+          }
+        });
+        const homeRef = ref(database, 'Home');
+    onValue(homeRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        if (data.igImg) {
+          setIgImg(`data:image/png;base64,${data.igImg}`);
+        }
+        if (data.linkedinImg) {
+          setLinkedInImg(`data:image/png;base64,${data.linkedinImg}`);
+        }
+        if (data.gihubImg) { 
+          setGithubImg(`data:image/jpeg;base64,${data.gihubImg}`);
+        }
+      }
+    });
+    setTimeout(() => setVisible(true), 100); 
+}, []);
+
   return (
     <div id="contact">
-        <h2 className="section_title">CONTACT</h2>
-        <h2 className="section_subtitle">My Contact</h2>
+        <h2 className="section_title">{contactTitle}</h2>
+        <h2 className="section_subtitle">{contactSubtitle}</h2>
         
         <div className="social">
           <div className='contact_item'>
-            <a href='elshatambuwun20@gmail.com' className='icon-email contact_social-icon'></a>
-            <p className='contact_text'>elshatambuwun20@gmail.com</p>
+            <a 
+            href='elshatambuwun20@gmail.com' 
+            className='icon-email contact_social-icon' 
+            target='_blank'
+            style={{ backgroundImage: `url(${emailImg})` }}
+            ></a>
+            <p className='contact_text'>{email}</p>
           </div>
           
           <div className='contact_item'>
-            <a href='https://github.com/elshaaddai' className='icon-github contact_social-icon'></a>
-            <p className='contact_text'>Github</p>
+            <a 
+            href='https://github.com/elshaaddai' 
+            className='icon-github contact_social-icon' 
+            target='_blank'
+            style={{ backgroundImage: `url(${gihubImg})` }}
+            ></a>
+            <p className='contact_text'>{github}</p>
           </div>
 
           <div className='contact_item'>
-            <a href='https://www.linkedin.com/in/elshaddai-grace-tambuwun-355899304/' className='icon-linkedin contact_social-icon'></a>
-            <p className='contact_text'>LinkedIn</p>
+            <a 
+            href='https://www.linkedin.com/in/elshaddai-grace-tambuwun-355899304/' 
+            className='icon-linkedin contact_social-icon' 
+            target='_blank'
+            style={{ backgroundImage: `url(${linkedinImg})` }}
+            ></a>
+            <p className='contact_text'>{linkedIn}</p>
+          </div>
+
+          <div className='contact_item'>
+            <a 
+            href='https://www.instagram.com/elshaa.tambuwun/#' 
+            className='icon-instagram contact_social-icon' 
+            target='_blank'
+            style={{ backgroundImage: `url(${igImg})` }}
+            ></a>
+            <p className='contact_text'>{instagram}</p>
           </div>
 
 
